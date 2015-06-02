ee980640b43f89dea42a2bf46f47d0dd63769bd1
Andrew Seward <awswrd@gmail.com>
2015-06-01 21:11:06 -0700

    Initial commit Everything from project scaffold...

---
+++ src/FsharpNetInterop/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FsharpNetInterop")>]
+[<assembly: AssemblyProductAttribute("FsharpNetInterop")>]
+[<assembly: AssemblyDescriptionAttribute("Utilities for interoperability with .NET languages")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
